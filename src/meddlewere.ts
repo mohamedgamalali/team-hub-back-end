f4f39ec0140f1029d7777da897895fc2673bf837
mohamedgamalali <mg7404809@gmail.com>
2021-09-24 19:22:14 +0200

    edit profile

--- src/meddlewere.ts
+++ src/meddlewere.ts
@@ -51,7 +51,7 @@ export default (app: Application) => {
     //headers meddlewere
     app.use((req: Request, res: Response, next: NextFunction) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
         next();
     });
